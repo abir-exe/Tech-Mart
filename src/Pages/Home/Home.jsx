eeb2d5777c6f66c607312bf877e184f35b09a5b7
Abir <abirmahmud221@gmail.com>
2023-10-19 18:36:52 +0600

    added the json file for home page and fetched data using useloader

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,30 @@
+import { useLoaderData } from "react-router-dom";
 import AboutUs from "./HomeComponents/AboutUs";
 import Banner from "./HomeComponents/Banner";
 import Brands from "./HomeComponents/Brands";
 import Featured from "./HomeComponents/Featured";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <h2 className="text-3xl">This is Home</h2>
-            <Brands></Brands>
-            <Featured></Featured>
-            <AboutUs></AboutUs>
+  const brands = useLoaderData();
+  console.log(brands);
+
+  return (
+    <div>
+      <Banner></Banner>
+
+      <div className="my-10">
+        <h2 className="text-2xl text-center my-10">Shop By Brands</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5 my-5">
+          {brands.map((brand) => (
+            <Brands key={brand.id} brand={brand}></Brands>
+          ))}
         </div>
-    );
+      </div>
+
+      <Featured></Featured>
+      <AboutUs></AboutUs>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/Home/HomeComponents/Brands.jsx
+++ src/Pages/Home/HomeComponents/Brands.jsx
@@ -1,89 +1,22 @@
 import appleLogo from "../../../../public/images/Apple-6176.png";
 
-const Brands = () => {
+const Brands = ({brand}) => {
+
+  const {id, name, image} = brand || {};
+  console.log(brand);
+
   return (
     <div className="my-10">
-      <h2 className="text-2xl text-center my-10">Shop By Brands</h2>
+      
 
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 my-5">
+      <div >
         {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
+        <div className="card bg-base-100 shadow-xl h-52">
           <figure>
-            <img src={appleLogo} alt="Shoes" />
+            <img className="p-1" src={image} alt="Shoes" />
           </figure>
           <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-          </div>
-        </div>
-        {/* card */}
-        <div className="card   bg-base-100 shadow-xl">
-          <figure>
-            <img src={appleLogo} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Apple</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="text-bold text-xl text-center p-2">{name}</h2>
           </div>
         </div>
       </div>
