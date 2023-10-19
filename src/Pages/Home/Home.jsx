b48eaef6d11b527968e2e286784ffbb629947651
Abir <abirmahmud221@gmail.com>
2023-10-19 12:49:24 +0600

    navbar and header design completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import Navbar from "../Shared/Navbar/Navbar";
+import Header from "./HomeComponents/Header";
 
 const Home = () => {
     return (
         <div>
+            <Navbar/>
+            <Header></Header>
             <h2 className="text-3xl">This is Home</h2>
         </div>
     );
---
+++ src/Pages/Home/HomeComponents/Header.jsx
@@ -0,0 +1,33 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <div className="bg-[#ffffff] px-1 py-3">
+                <ul className="flex justify-between items-center">
+                <li>Desktop</li>
+                <li>Laptop</li>
+                <li>Component</li>
+                <li>Monitor</li>
+                <li>UPS</li>
+                <li>Phone</li>
+                <li>Tablet</li>
+                <li>Office</li>
+                <li>Camera</li>
+                <li>Software</li>
+                <li>Gadget</li>
+                <li>Storage</li>
+                <li>TV</li>
+                <li>Gaming</li>
+                <li>AC</li>
+                <li>Security</li>
+                <li>Accessories</li>
+
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
